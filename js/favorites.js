8af98ebfcf3786908179b780be1d12323781836e
Fernando <fernando_ruans@hotmail.com>
2023-10-31 10:28:26 -0300

    Promessas com Async Await.

--- js/favorites.js
+++ js/favorites.js
@@ -20,13 +20,15 @@ export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
-
-        GithubUser.search('fernando-ruans').then(user => console.log(user))
     }
 
 
     load() {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || [] 
+    }
+
+    async add(username) {
+        const user = await GithubUser.search(username)
     }
 
     delete(user) {
@@ -47,6 +49,16 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const {value} = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
